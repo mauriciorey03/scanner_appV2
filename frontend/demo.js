9974eb6c8bb8e75f36e8ac64a143a02700838f56
mauriciorey03 <edgarmeza909@gmail.com>
2025-06-06 02:49:18 -0500

    update index

---
+++ frontend/demo.js
@@ -0,0 +1,123 @@
+//demo.js
+const ejemplos = [
+    {
+        "titular": "Consumidor Final",
+        "documento": "2222222222",
+        "tienda": "CADENA COMERCIAL OXXO COLOMBIA S.A.S",
+        "NIT": "900262579",
+        "contacto": null,
+        "fecha_compra": "2023-05-16",
+        "valor_total": "10,200",
+        "prefijo": "2161 180422"
+    },
+    {
+        "titular": "Supermercado XYZ S.A.S.",
+        "documento": "1098670285",
+        "tienda": "Supermercado XYZ S.A.S.",
+        "NIT": "900123456",
+        "contacto": "3001234567",
+        "fecha_compra": "2025-03-03",
+        "valor_total": "22,000.50",
+        "prefijo": "POS1881"
+    },
+    {
+        "titular": "Juan Perez",
+        "documento": "1020304050",
+        "tienda": "TIENDAS D1 S.A.S.",
+        "NIT": "800900123",
+        "contacto": null,
+        "fecha_compra": "2024-06-12",
+        "valor_total": "5,500",
+        "prefijo": "3470 005621"
+    },
+    {
+        "titular": "Droguería Nacional S.A.",
+        "documento": "1053482765",
+        "tienda": "DROGUERÍA NACIONAL S.A.",
+        "NIT": "900456789",
+        "contacto": "info@droguerianacional.com",
+        "fecha_compra": "2024-05-09",
+        "valor_total": "48,900",
+        "prefijo": "R-4521"
+    },
+    {
+        "titular": "Carlos Méndez",
+        "documento": "1009876543",
+        "tienda": "ÉXITO S.A.",
+        "NIT": "890900943",
+        "contacto": "3209876543",
+        "fecha_compra": "2023-11-15",
+        "valor_total": "156,230",
+        "prefijo": "EX-8204"
+    },
+    {
+        "titular": "Andrea Giraldo",
+        "documento": "1144112233",
+        "tienda": "ALKOSTO S.A.S.",
+        "NIT": "900222334",
+        "contacto": null,
+        "fecha_compra": "2024-01-27",
+        "valor_total": "79,500",
+        "prefijo": "AK-2019"
+    },
+    {
+        "titular": "Miguel Torres",
+        "documento": "1011002223",
+        "tienda": "TIENDAS ARA S.A.S.",
+        "NIT": "900302113",
+        "contacto": "3013344556",
+        "fecha_compra": "2024-04-02",
+        "valor_total": "13,750",
+        "prefijo": "ARA-7801"
+    },
+    {
+        "titular": "Consumidor Final",
+        "documento": "1199887766",
+        "tienda": "JUMBO COLOMBIA S.A.",
+        "NIT": "860002405",
+        "contacto": "servicio@jumbo.com.co",
+        "fecha_compra": "2023-10-22",
+        "valor_total": "208,600",
+        "prefijo": "JU-3345"
+    },
+    {
+        "titular": "Olga Rueda",
+        "documento": "1044118855",
+        "tienda": "OLÍMPICA S.A.",
+        "NIT": "890100213",
+        "contacto": null,
+        "fecha_compra": "2024-07-06",
+        "valor_total": "34,200",
+        "prefijo": "OLI-2321"
+    },
+    {
+        "titular": "José Martínez",
+        "documento": "1122446688",
+        "tienda": "TIENDA MERCADO POPULAR",
+        "NIT": "101232145",
+        "contacto": "3129988776",
+        "fecha_compra": "2023-09-12",
+        "valor_total": "4,850",
+        "prefijo": "MP-9190"
+    },
+    {
+        "titular": "Maria Fernanda Ruiz",
+        "documento": "1102233445",
+        "tienda": "PEPE GANGA S.A.",
+        "NIT": "900120045",
+        "contacto": null,
+        "fecha_compra": "2024-02-18",
+        "valor_total": "95,000",
+        "prefijo": "PG-2024"
+    },
+    {
+        "titular": "Consumidor Final",
+        "documento": "1033300001",
+        "tienda": "SÚPER INTER S.A.S.",
+        "NIT": "900220011",
+        "contacto": null,
+        "fecha_compra": "2024-03-31",
+        "valor_total": "17,300",
+        "prefijo": "SI-1048"
+    }
+];
